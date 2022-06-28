89db470a88d25ca234c1f016e8f1ab77dd7ec8d5
Your Name <you@example.com>
2022-06-28 21:31:20 +0530

    h

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb://mongo:VLD3xqMnXNiAtkfz2Ylu@containers-us-west-48.railway.app:6753',//database link
+    
+    bot_token:'5488204309:AAFkeqJ_-VMHuWLzZq6gC3-4YKgCFJAtjvc',//bot token
+    
+    bot_name:'Polygongggggbot',//bot username without @
+    
+    bot_admin:5159732126,// admin telegram id
+    
+    channelsList : ['@fjfndndnfbt'],// channels list
+    
+    reffer_bonus:5,// refferal bonus amount
+    
+    min_wd:1,//min withdrawal amount
+    
+    daily_bonus:100,// daily bonus amount
+    
+    currency:'tro', //bot currency
+    
+    contractaddress :  '0x43a927aa7927fce2588025cccb13a8fd0632c059', //contract address of coin
+    
+    privateKey : '03d0fbc7d8633d1021aa2af3c0f2a5220e9fc2ef2185b2444f15d2cc7b30338a', //private key of wallet
+    
+    address : '0x0d133ce1f292C8f41674b1ac79fbB630a7230f03',//polygon address of wallet
+    
+    payment_channel:'@fjfndndnfbt'// payment channel username
+    
+    }
